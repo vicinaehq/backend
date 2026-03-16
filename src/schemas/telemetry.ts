1c63cc0f960fc209f448dbdf59f1bffa0b93f4a8
Aurelien Brabant <gh@aurelle.dev>
2026-03-16 19:46:45 +0100

    fix: qt version is optional

--- src/schemas/telemetry.ts
+++ src/schemas/telemetry.ts
@@ -26,7 +26,7 @@ export const systemInfoSchema = z.object({
 	kernelVersion: z.string().min(1).max(128),
 	productId: z.string().min(1).max(128),
 	productVersion: z.string().min(1).max(128),
-	qtVersion: z.string().regex(/^\d+\.\d+\.\d+$/),
+	qtVersion: z.string().regex(/^\d+\.\d+\.\d+$/).optional()
 });
 
 export type SystemInfoPayload = z.infer<typeof systemInfoSchema>;
